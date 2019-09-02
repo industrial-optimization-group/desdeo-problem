61f8ec579fe8df6944b62d10ca82bc7176c84ac4
Bhupinder Saini <rishi@pop-os.localdomain>
2019-09-02 15:18:35 +0300

    added examples

--- examples/AnalyticalProblem.py
+++ examples/AnalyticalProblem.py
@@ -4,6 +4,7 @@ from desdeo_problem.Objective import ScalarObjective, VectorObjective
 from desdeo_problem.Problem import MOProblem
 from desdeo_problem.Variable import variable_builder
 
+
 var_names = ["a", "b", "c"]
 initial_values = [1, 1, 1]
 lower_bounds = [0, -1, 0]
@@ -24,6 +25,7 @@ def obj3(x):
 
 f1_2 = VectorObjective(["y1", "y2"], obj1_2)
 f3 = ScalarObjective("f3", obj3)
+
 prob = MOProblem([f1_2, f3], variables, None)
 data = np.asarray([[1, 1, 1], [1, -1, 0]])
 print(prob.evaluate(data))
