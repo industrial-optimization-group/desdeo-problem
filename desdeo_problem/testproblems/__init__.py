5ceda886baf066ba738601a4e91fa8895f627234
jpajasmaa <juuso.pajasmaa@gmail.com>
2021-10-04 16:03:11 +0300

    maybe like this

--- desdeo_problem/testproblems/__init__.py
+++ desdeo_problem/testproblems/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ["test_problem_builder", "DBMOPP", "Region"]
+__all__ = ["test_problem_builder", "DBMOPP", "Region", "utilities"]
 
 from desdeo_problem.testproblems.TestProblems import test_problem_builder
 
 from desdeo_problem.testproblems.DBMOPP import DBMOPP
 from desdeo_problem.testproblems.Region import Region
+from desdeo_problem.testproblems.utilities import utilities
