55e175f4ce199691ac7fe28437d4adf9a5d18e10
jpajasmaa <juuso.pajasmaa@gmail.com>
2021-10-04 16:49:22 +0300

    this fix

--- desdeo_problem/testproblems/Region.py
+++ desdeo_problem/testproblems/Region.py
@@ -1,4 +1,4 @@
-from utilities import *
+from desdeo_problem.testproblems.utilities import get_2D_version, euclidean_distance, convhull, in_hull, get_random_angles, between_lines_rooted_at_pivot, assign_design_dimension_projection
 from typing import Dict 
 import numpy as np
 from matplotlib.patches import Circle
