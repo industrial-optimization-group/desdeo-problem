0382b4b93892c39d8944b69c7883afcc1676838b
jpajasmaa <juuso.pajasmaa@gmail.com>
2021-10-04 15:33:24 +0300

    missed this

--- desdeo_problem/testproblems/__init__.py
+++ desdeo_problem/testproblems/__init__.py
@@ -1,7 +1,6 @@
-__all__ = ["test_problem_builder", "DBMOPP", "Region", "utilities"]
+__all__ = ["test_problem_builder", "DBMOPP", "Region"]
 
 from desdeo_problem.testproblems.TestProblems import test_problem_builder
 
 from desdeo_problem.testproblems.DBMOPP import DBMOPP
 from desdeo_problem.testproblems.Region import Region
-from desdeo_problem.testproblems.utilities import utilities
