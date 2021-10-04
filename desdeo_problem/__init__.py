468f029ffe692e026f397719d93f1149a350d4f1
jpajasmaa <juuso.pajasmaa@gmail.com>
2021-10-04 15:10:21 +0300

    fix

--- desdeo_problem/__init__.py
+++ desdeo_problem/__init__.py
@@ -33,6 +33,7 @@ __all__ = [
     "test_problem_builder",
     "DBMOPP",
     "Region",
+    "utilities",
 ]
 
 from desdeo_problem.problem import (
@@ -74,4 +75,5 @@ from desdeo_problem.testproblems.TestProblems import test_problem_builder
 
 from desdeo_problem.testproblems.DBMOPP import DBMOPP
 from desdeo_problem.testproblems.Region import Region
+from desdeo_problem.testproblems.utilities import utilities
 
