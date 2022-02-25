4eaee1b2b920def5617963f20a667df9f59e94a7
jpajasmaa <juuso.pajasmaa@gmail.com>
2022-02-25 11:14:42 +0200

    fixed faulty test_testproblems

--- tests/test_TestProblems.py
+++ tests/test_TestProblems.py
@@ -1,22 +1,45 @@
 from desdeo_problem import MOProblem
 from desdeo_problem.testproblems.TestProblems import test_problem_builder
+import pytest
+# smh wrong with importing test_problem_builder..
+
+# fixtures are kind of parameters to give to the test functions like test_problem_builder
 
 
-problem_name = "ZDT1"
-problem = test_problem_builder(problem_name)
+@pytest.fixture
+def name():
+    name = "ZDT1"
+    return name
 
-problem_name = "DTLZ1" 
-problem = test_problem_builder(problem_name, n_of_variables=7, n_of_objectives=3)
-    
-problem_name = "DTLZ2" 
-problem = test_problem_builder(problem_name, n_of_variables=12, n_of_objectives=3)
 
-problem_name = "DTLZ4"
-problem = test_problem_builder(problem_name, n_of_variables=12, n_of_objectives=3)
+def test_zdt(name):
+    prob = test_problem_builder(name)
+    assert type(prob) == MOProblem
 
-problem_name = "DTLZ6"
-problem = test_problem_builder(problem_name, n_of_variables=12, n_of_objectives=3)
 
-problem_name = "DTLZ7"
-problem = test_problem_builder(problem_name, n_of_variables=22, n_of_objectives=3)
-print("success")
+# problem_name = "DTLZ1"
+# problem = test_problem_builder(problem_name, n_of_variables=7, n_of_objectives=3)
+# problem_name = "DTLZ2"
+# problem = test_problem_builder(problem_name, n_of_variables=12, n_of_objectives=3)
+# problem_name = "DTLZ4"
+# problem = test_problem_builder(problem_name, n_of_variables=12, n_of_objectives=3)
+# problem_name = "DTLZ6"
+# problem = test_problem_builder(problem_name, n_of_variables=12, n_of_objectives=3)
+# problem_name = "DTLZ7"
+# problem = test_problem_builder(problem_name, n_of_variables=22, n_of_objectives=3)
+
+# tälle voi antaa tavaraa. "param nimet", aloitusarvot, lopetusarvot, esim. x*y == z
+# @pytest.mark.parametrize("x,y,z", [(10,20,200), (20,40,200)])
+
+
+@pytest.fixture
+def dtlz_params():
+    name = "DTLZ4"
+    vars = 12
+    objs = 3
+    return [name, vars, objs]
+
+
+def test_dtlz(dtlz_params):
+    prob = test_problem_builder(dtlz_params[0], dtlz_params[1], dtlz_params[2])
+    assert type(prob) == MOProblem
