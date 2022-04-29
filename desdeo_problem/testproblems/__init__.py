a3364f813cb7510d0373f11e70683dce3eb8bc86
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2022-04-29 14:58:36 +0300

    Added a dummy problem to showcase how problems should be implemented in testproblems. Also added a test to test the problem.

--- desdeo_problem/testproblems/__init__.py
+++ desdeo_problem/testproblems/__init__.py
@@ -1,9 +1,10 @@
-__all__ = ["test_problem_builder"]
+__all__ = ["test_problem_builder", "dummy_problem"]
 
 
 # __all__ = ["test_problem_builder", "DBMOPP_generator", "Region", "get_2D_version", "euclidean_distance", "convhull", "in_hull", "get_random_angles", "between_lines_rooted_at_pivot", "assign_design_dimension_projection"]
 
 from desdeo_problem.testproblems.TestProblems import test_problem_builder
+from desdeo_problem.testproblems.DummyProblem import dummy_problem
 
 # from desdeo_problem.testproblems.DBMOPP.DBMOPP_generator import DBMOPP_generator
 # from desdeo_problem.testproblems.DBMOPP.Region import Region
