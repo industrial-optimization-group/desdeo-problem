d0cd866f88fbd807898d97f907f783c19c410184
Matti Viitanen <maaviixu@it-koru-2204.it.jyu.fi>
2022-05-24 09:29:54 +0300

    Fixed clarity of names

--- desdeo_problem/testproblems/EngineeringRealWorld.py
+++ desdeo_problem/testproblems/EngineeringRealWorld.py
@@ -5,13 +5,25 @@ from desdeo_problem import ScalarConstraint
 
 import numpy as np
 
+"""
+A real-world multi-objective problem suite (the RE benchmark set)
+
+Tanabe, R. & Ishibuchi, H. (2020). An easy-to-use real-world multi-objective 
+optimization problem suite. Applied soft computing, 89, 106078. 
+https://doi.org/10.1016/j.asoc.2020.106078 
+
+https://github.com/ryojitanabe/reproblems/blob/master/reproblem_python_ver/reproblem.py
+
+"""
+
 def re21(var_iv: np.array = np.array([2, 2, 2, 2])) -> MOProblem:
-    """ Four bar truss design problem
+    """ Four bar truss design problem. 
+    Two objectives and four variables.
     
     Arguments:
         var_iv (np.array): Optional, initial variable values.
-            Defaults are [2, 2, 2, 2]. x1 and x4 lbound is 1, x2 and x3 lb is sqrt(2),
-            ubound for all xs is 3.
+            Defaults are [2, 2, 2, 2]. x1, x4 ∈ [a, 3a], x2, x3 ∈ [√2 a, 3a]
+            and a = F / sigma
     Returns:
         MOProblem: a problem object.
     """
@@ -20,8 +32,6 @@ def re21(var_iv: np.array = np.array([2, 2, 2, 2])) -> MOProblem:
     sigma = 10.0
     E = 2.0 * 1e5
     L = 200.0
-    
-    # a selkeyttämään koodia
     a = F / sigma
 
     # Lower bounds
@@ -29,11 +39,9 @@ def re21(var_iv: np.array = np.array([2, 2, 2, 2])) -> MOProblem:
     # Upper bounds
     ub = np.array([3 * a, 3 * a, 3 * a, 3 * a])
 
-    # Pitäisikö aikaisemmat muuttaa myös np.any?
-    # Check variable bounds
+    # Check the variable bounds
     if np.any(lb > var_iv) or np.any(ub < var_iv):
         raise ValueError("Initial variable values need to be between lower and upper bounds")
-
 
     def f_1(x: np.ndarray) -> np.ndarray:
         x = np.atleast_2d(x)
@@ -44,11 +52,12 @@ def re21(var_iv: np.array = np.array([2, 2, 2, 2])) -> MOProblem:
         return ((F * L) / E) * ((2.0 / x[:, 0]) + 
             (2.0 * np.sqrt(2.0) / x[:, 1]) - (2.0 * np.sqrt(2.0) / x[:, 2]) + (2.0 / x[:, 3]))
 
-    objective_1 = ScalarObjective(name="name", evaluator=f_1, maximize=[False])
-    objective_2 = ScalarObjective(name="name", evaluator=f_2, maximize=[False])
+    objective_1 = ScalarObjective(name="minimize the structural volume", evaluator=f_1, maximize=[False])
+    objective_2 = ScalarObjective(name="minimize the joint displacement", evaluator=f_2, maximize=[False])
 
     objectives = [objective_1, objective_2]
 
+    # The four variables determine the length of four bars
     x_1 = Variable("x_1", 2 * a, a, 3 * a)
     x_2 = Variable("x_2", 2 * a, (np.sqrt(2.0) * a), 3 * a)
     x_3 = Variable("x_3", 2 * a, (np.sqrt(2.0) * a), 3 * a)
@@ -60,18 +69,42 @@ def re21(var_iv: np.array = np.array([2, 2, 2, 2])) -> MOProblem:
 
     return problem
 
-def re22(var_iv: np.array = np.array([7, 10, 20])) -> MOProblem:
+def re22(var_iv: np.array = np.array([7.2, 10, 20])) -> MOProblem:
     """ Reinforced concrete beam design problem.
+    Two objectives and three variables.
     
     Arguments:
         var_iv (np.array): Optional, initial variable values.
-            Defaults are [7, 10, 20]. Bounds: x1 [0.2, 15], x2 [0, 20], x3 [0, 40]
+            Defaults are [7.2, 10, 20]. x2 ∈ [0, 20] and x3 ∈ [0, 40].
+            x1 has a pre-defined discrete value from 0.2 to 15.
     Returns:
         MOProblem: a problem object.
     """
 
+    # Lower bounds
+    lb = np.array([0.2, 0, 0])
+    # Upper bounds
+    ub = np.array([15, 20, 40])
+
+    # Check the variable bounds
+    if np.any(lb > var_iv) or np.any(ub < var_iv):
+        raise ValueError("Initial variable values need to be between lower and upper bounds")
+
+    # x1 pre defined discrete values
+    feasible_vals = np.array([0.20, 0.31, 0.40, 0.44, 0.60, 0.62, 0.79, 0.80, 0.88, 0.93,
+                            1.0, 1.20, 1.24, 1.32, 1.40, 1.55, 1.58, 1.60, 1.76, 1.80,
+                            1.86, 2.0, 2.17, 2.20, 2.37, 2.40, 2.48, 2.60, 2.64, 2.79,
+                            2.80, 3.0, 3.08, 3.10, 3.16, 3.41, 3.52, 3.60, 3.72, 3.95,
+                            3.96, 4.0, 4.03, 4.20, 4.34, 4.40, 4.65, 4.74, 4.80, 4.84,
+                            5.0, 5.28, 5.40, 5.53, 5.72, 6.0, 6.16, 6.32, 6.60, 7.11,
+                            7.20, 7.80, 7.90, 8.0, 8.40, 8.69, 9.0, 9.48, 10.27, 11.0,
+                            11.06, 11.85, 12.0, 13.0, 14.0, 15.0])
+
     def g_1(x: np.ndarray, _ = None) -> np.ndarray:
         x = np.atleast_2d(x)
+        fv_2d = np.repeat(np.atleast_2d(feasible_vals), x.shape[0], axis=0)
+        idx = np.abs(fv_2d.T - x[:, 0]).argmin(axis=0)
+        x[:, 0] = feasible_vals[idx]
         return x[:, 0] * x[:, 2] - 7.735 * (x[:, 0]**2 / x[:, 1]) - 180
 
     def g_2(x: np.ndarray, _ = None) -> np.ndarray:
@@ -80,22 +113,21 @@ def re22(var_iv: np.array = np.array([7, 10, 20])) -> MOProblem:
 
     def f_1(x: np.ndarray) -> np.ndarray:
         x = np.atleast_2d(x)
+        fv_2d = np.repeat(np.atleast_2d(feasible_vals), x.shape[0], axis=0)
+        idx = np.abs(fv_2d.T - x[:, 0]).argmin(axis=0)
+        x[:, 0] = feasible_vals[idx]
         return (29.4 * x[:, 0]) + (0.6 * x[:, 1] * x[:,2])
 
-    # todo summaa max{g(i), 0}
-    # eli jos g_1 tai g_2 saa negatiivisen arvon summataan 0
-    # lopputulos oltava pos
     def f_2(x: np.ndarray) -> np.ndarray:
         x = np.atleast_2d(x)
-        sum = 0
-        if g_1(x) > 0:
-            sum =+ g_1(x)
-        if g_2(x) > 0:
-            sum =+ g_2(x) 
-        return sum
+        sum1 = g_1(x)
+        sum2 = g_2(x)
+        sum1 = np.where(sum1 > 0, sum1, 0)
+        sum2 = np.where(sum2 > 0, sum2, 0)
+        return sum1 + sum2
 
-    objective_1 = ScalarObjective(name="name", evaluator=f_1, maximize=[False])
-    objective_2 = ScalarObjective(name="name", evaluator=f_2, maximize=[False])
+    objective_1 = ScalarObjective(name="minimize the total cost of concrete", evaluator=f_1, maximize=[False])
+    objective_2 = ScalarObjective(name="minimize the reinforcing steel of the beam", evaluator=f_2, maximize=[False])
 
     objectives = [objective_1, objective_2]
 
@@ -104,12 +136,9 @@ def re22(var_iv: np.array = np.array([7, 10, 20])) -> MOProblem:
 
     constraints = [cons_1, cons_2]
 
-    # todo x_1:n arvot
-    # x_1 määritellyt arvot välillä 0.2 15. Löytyy koodista feasible_vals
-
-    x_1 = Variable("x_1", 7, 0.2, 15)
-    x_2 = Variable("x_2", 10, 0, 20)
-    x_3 = Variable("x_3", 20, 0, 40)
+    x_1 = Variable("the area of the reinforcement", 7.2, 0.2, 15)
+    x_2 = Variable("the width of the beam", 10, 0, 20)
+    x_3 = Variable("the depth of the beam", 20, 0, 40)
 
     variables = [x_1, x_2, x_3]
 
--- tests/test_engineering_real_world.py
+++ tests/test_engineering_real_world.py
@@ -1,20 +1,21 @@
 from desdeo_problem.testproblems.EngineeringRealWorld import *
-# kysy importataanko kaikki re... funktiot samaan testitiedostoon
 from desdeo_problem.problem import MOProblem
 import pytest
 import numpy as np
 import numpy.testing as npt
 
 @pytest.mark.re21
-def test_number_of_variables():
+def test_number_of_variables_re21():
     p: MOProblem = re21()
 
     assert p.n_of_variables == 4
 
+# Evaluating problem with some variable values
 @pytest.mark.re21
 def test_evaluate_re21():
     p: MOProblem = re21()
 
+    # Variable values
     xs = np.array([2, 2, 2, 2])
 
     objective_vectors = p.evaluate(xs).objectives
@@ -26,31 +27,43 @@ def test_evaluate_re21():
     npt.assert_allclose(objective_vectors, expected_result)
 
 @pytest.mark.re21
-def test_variable_bounds_error_1d():
+def test_variable_bounds_error_re21_1d():
     with pytest.raises(ValueError):
         p: MOProblem = re21(var_iv=np.array([1,2,3,4]))
 
 @pytest.mark.re21
-def test_variable_bounds_error_2d():
+def test_variable_bounds_error_re21_2d():
     with pytest.raises(ValueError):
         p: MOProblem = re21(var_iv=np.array([[1,2,2,2],[4,1,2,0]]))
 
 @pytest.mark.re22
-def test_number_of_variabls():
+def test_number_of_variables_re22():
     p: MOProblem = re22()
 
     assert p.n_of_variables == 3
 
+# Evaluating problem with some variable values
 @pytest.mark.re22
 def test_evaluate_re22():
     p: MOProblem = re22()
 
-    xs = np.array([[10, 10, 20], [12, 10, 20]])
+    # Variable values
+    xs = np.array([[10., 10., 20.], [12., 10., 20.], [11.5, 15, 35]])
 
     objective_vectors = p.evaluate(xs).objectives
 
-    assert objective_vectors.shape[0] == 1
+    assert objective_vectors.shape[0] == 3
 
-    expected_result = np.array([[414, 2], [472.8, 2]])
+    expected_result = np.array([[421.938, 2], [472.8, 2], [663.39, 164.0054642]])
 
     npt.assert_allclose(objective_vectors, expected_result)
+
+@pytest.mark.re22
+def test_variable_bounds_error_re22_1d():
+    with pytest.raises(ValueError):
+        p: MOProblem = re22(var_iv=np.array([1,-2,3]))
+
+@pytest.mark.re22
+def test_variable_bounds_error_re22_2d():
+    with pytest.raises(ValueError):
+        p: MOProblem = re22(var_iv=np.array([[1,2,2],[0,1,2]]))
